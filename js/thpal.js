1fc54e4179ae0aa84b8fc09da5fe83c25d7c557a
Transpofile <79129356+Transpofile@users.noreply.github.com>
2023-10-19 13:59:50 +0800

    Add files via upload

---
+++ js/thpal.js
@@ -0,0 +1,34 @@
+<script>
+
+/* 
+var circle = getCircleElement;
+
+function randomNumber(low, high) {
+  calculate random number low and high;
+  return number;
+}
+
+function newRGB() {
+  var newColor = "rgb(";
+  for(3 loops) {
+    var randomNumber = randomNumber();
+    newColor+= randomNumber;
+  }
+  newColor+= ")";
+  return newColor;
+}
+
+function updateColor() {
+  circle.bgColor = newRGB();
+}
+
+setTimeout() {
+  every 1.3 seconds...
+  updateColor();
+}
+
+*/
+
+
+
+</script>
